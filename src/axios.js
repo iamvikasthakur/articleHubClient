561df32a5a0e7a756fe085e62a7c87b84c89fea8
imavikasthakur <vikas.thakur@msit.in>
2021-02-13 00:08:46 +0530

    server url updated

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9000',
+    baseURL: 'https://articlehubb.herokuapp.com',
     // timeout: 10000,
     // headers: {'X-Custom-Header': 'foobar'}
 });
